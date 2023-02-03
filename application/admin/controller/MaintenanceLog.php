faf830d27f31842c5bb439d201163c2c820b28a0
hujiajie <hujiajje@gmail.com>
2023-02-03 15:51:50 +0800

    校验运维记录管理中device_no的有效性

--- application/admin/controller/MaintenanceLog.php
+++ application/admin/controller/MaintenanceLog.php
@@ -3,9 +3,11 @@
 namespace app\admin\controller;
 
 use app\common\controller\Backend;
+use Exception;
 use think\Db;
 use think\exception\PDOException;
 use think\exception\ValidateException;
+use think\Session;
 
 /**
  * 运维记录
@@ -39,6 +41,122 @@ class MaintenanceLog extends Backend
      * 需要将application/admin/library/traits/Backend.php中对应的方法复制到当前控制器,然后进行修改
      */
 
+
+    /**
+     * 添加
+     */
+    public function add()
+    {
+        if ($this->request->isPost()) {
+            $this->token();
+            $params = $this->request->post("row/a");
+            // 检查device_no 合理性
+            if (isset($params['device_no'])){
+                if ($params['device_no'] <= 0){
+                    $this->error('设备ID不能为负数，请填入设备管理功能中对应设备的设备ID');
+                }
+            }
+            if ($params) {
+                $params = $this->preExcludeFields($params);
+
+                $this->fzyq($params);
+                if ($this->dataLimit && $this->dataLimitFieldAutoFill) {
+                    $params[$this->dataLimitField] = $this->auth->id;
+                }
+                $result = false;
+                Db::startTrans();
+                try {
+                    //是否采用模型验证
+                    if ($this->modelValidate) {
+                        $name = str_replace("\\model\\", "\\validate\\", get_class($this->model));
+                        $validate = is_bool($this->modelValidate) ? ($this->modelSceneValidate ? $name . '.add' : $name) : $this->modelValidate;
+                        $this->model->validateFailException(true)->validate($validate);
+                    }
+                    $result = $this->model->allowField(true)->save($params);
+                    Db::commit();
+                } catch (ValidateException $e) {
+                    Db::rollback();
+                    $this->error($e->getMessage());
+                } catch (PDOException $e) {
+                    Db::rollback();
+                    $this->error($e->getMessage());
+                } catch (Exception $e) {
+                    Db::rollback();
+                    $this->error(Session::get('fzyq_error_info')??$e->getMessage());
+                }
+                if ($result !== false) {
+                    $this->success();
+                } else {
+                    $this->error(__('No rows were inserted'));
+                }
+            }
+            $this->error(__('Parameter %s can not be empty', ''));
+        }
+        return $this->view->fetch();
+    }
+
+    /**
+     * 编辑
+     */
+    public function edit($ids = null)
+    {
+        $row = $this->model->get($ids);
+        if (!$row) {
+            $this->error(__('No Results were found'));
+        }
+        $adminIds = $this->getDataLimitAdminIds();
+        if (is_array($adminIds)) {
+            if (!in_array($row[$this->dataLimitField], $adminIds)) {
+                $this->error(__('You have no permission'));
+            }
+        }
+        if ($this->request->isPost()) {
+            $this->token();
+            $params = $this->request->post("row/a");
+            // 检查device_no 合理性
+            if (isset($params['device_no'])){
+                if ($params['device_no'] <= 0){
+                    $this->error('设备ID必须是正数，请填入设备管理功能中对应设备的设备ID');
+                }
+            }
+            if ($params) {
+                $this->fzyq($params);
+
+                $params = $this->preExcludeFields($params);
+                $result = false;
+                Db::startTrans();
+                try {
+                    //是否采用模型验证
+                    if ($this->modelValidate) {
+                        $name = str_replace("\\model\\", "\\validate\\", get_class($this->model));
+                        $validate = is_bool($this->modelValidate) ? ($this->modelSceneValidate ? $name . '.edit' : $name) : $this->modelValidate;
+                        $row->validateFailException(true)->validate($validate);
+                    }
+
+                    $result = $row->allowField(true)->save($params);
+                    Db::commit();
+                } catch (ValidateException $e) {
+                    Db::rollback();
+                    $this->error($e->getMessage());
+                } catch (PDOException $e) {
+                    Db::rollback();
+                    $this->error($e->getMessage());
+                } catch (Exception $e) {
+                    Db::rollback();
+                    $this->error(Session::get('fzyq_error_info')??$e->getMessage());
+                }
+                if ($result !== false) {
+                    $this->success();
+                } else {
+                    $this->error(__('No rows were updated'));
+                }
+            }
+            $this->error(__('Parameter %s can not be empty', ''));
+        }
+        $this->fzyq($row);
+        $this->view->assign("row", $row);
+        return $this->view->fetch();
+    }
 
 
 
